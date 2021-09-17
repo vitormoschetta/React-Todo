af3a92bd6eac0ececd7d3dcee83ec8a2a7eddd32
Vitor <vfarias@bionexo.com>
2021-09-16 21:51:07 -0400

    update files

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import reportWebVitals from './reportWebVitals';
 import App from './App';
 
-import './services/firebase';
+import './services/Firebase';
 
 ReactDOM.render(
   <React.StrictMode>    
@@ -12,14 +11,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-
-
-
-
-
-// Se você deseja começar a medir o desempenho em seu aplicativo, passe uma função
-// para registrar os resultados (por exemplo: reportWebVitals (console.log))
-// ou envie para um endpoint analítico. Saiba mais:
-// https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/setupTests.ts
+++
@@ -1,5 +0,0 @@
-// jest-dom adiciona matchers jest customizados para declaração em nós DOM.
-// permite que você faça coisas como:
-// expect (element) .toHaveTextContent (/ react / i)
-// saiba mais: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
